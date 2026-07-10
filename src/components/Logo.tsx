2d2dd5dac49f1b2094f375b95acfc83f03aa3053
codeRJ85 <126911946+codeRJ85@users.noreply.github.com>
2026-07-10 20:02:38 +0530

    feat: update brand logo design
    Refine the SVG paths and update color palette for a cleaner brand appearance.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -15,39 +15,14 @@ export default function Logo({ className = 'h-11 w-11' }: LogoProps) {
     >
       {/* Top light-blue shape */}
       <path
-        d="M 10,10 
-           H 60 
-           C 75,10 87,22 87,37 
-           C 87,41 85,44 82,46
-           C 62,41 42,43 10,51 
-           V 10 
-           Z 
-           M 26,26 
-           H 52 
-           C 59,26 64,31 64,37 
-           C 64,39 62,41 58,41
-           C 48,39 37,39 26,41 
-           V 26 
-           Z"
-        fill="#5EBCD3"
+        d="M 12,47 V 12 H 55 C 75,12 85,22 85,42 C 65,39 45,39 12,47 Z M 32,30 H 52 C 58,30 62,34 62,46 C 52,43 42,43 32,46 Z"
+        fill="#4FAEE3"
         fillRule="evenodd"
       />
       {/* Bottom dark-blue shape with swoosh extension & inner hole */}
       <path
-        d="M 10,68 
-           V 87 
-           H 60 
-           C 75,87 87,75 87,60 
-           C 87,55 91,51 95,47.5 
-           C 80,48 55,56 10,68 
-           Z 
-           M 26,70 
-           V 77 
-           C 26,77 40,81 53,77 
-           C 59,74 65,70 65,65 
-           C 55,66 40,68 26,70 
-           Z"
-        fill="#0050B3"
+        d="M 12,53 V 88 H 55 C 80,88 82,60 92,49 C 80,47 62,45 50,45 C 40,45 25,47 12,53 Z M 32,52 V 70 H 52 C 58,70 62,66 62,52 C 52,49 42,49 32,52 Z"
+        fill="#0055B8"
         fillRule="evenodd"
       />
     </svg>
